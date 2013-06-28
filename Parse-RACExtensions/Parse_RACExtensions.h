68e5a8a432ab1ee1cba7dc00ce41f1e8b1eef0ef
Dave Lee <davelee.com@gmail.com>
2013-06-28 11:52:17 -0600

    Initial Commit

---
+++ Parse-RACExtensions/Parse_RACExtensions.h
@@ -0,0 +1,13 @@
+//
+//  Parse_RACExtensions.h
+//  Parse-RACExtensions
+//
+//  Created by Dave Lee on 2013-06-28.
+//  Copyright (c) 2013 Dave Lee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Parse_RACExtensions : NSObject
+
+@end
---
+++ Parse-RACExtensionsTests/Parse_RACExtensionsTests.m
@@ -0,0 +1,32 @@
+//
+//  Parse_RACExtensionsTests.m
+//  Parse-RACExtensionsTests
+//
+//  Created by Dave Lee on 2013-06-28.
+//  Copyright (c) 2013 Dave Lee. All rights reserved.
+//
+
+#import "Parse_RACExtensionsTests.h"
+
+@implementation Parse_RACExtensionsTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Parse-RACExtensionsTests");
+}
+
+@end
