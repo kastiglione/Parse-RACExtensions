141357fd3e5f97202ce3084a18c6192a6932bd0b
Dave Lee <davelee.com@gmail.com>
2013-07-05 23:39:37 -0600

    Introduce default error messages for the account management API
    Parse's sample app AnyWall has code to handle these cases, so I'm building it
    into this library for convenience.
    
    See: https://github.com/ParsePlatform/AnyWall/blob/b961ceb8ea514b91ff5dea075b142545af5c21b8/AnyWall/PAWLoginViewController.m#L189

--- Parse-RACExtensions/PFUser+RACExtensions.m
+++ Parse-RACExtensions/PFUser+RACExtensions.m
@@ -10,31 +10,41 @@
 
 #import "PFUser+RACExtensions.h"
 #import "PFRACCallbacks.h"
+#import "PFRACErrors.h"
 
 @implementation PFUser (RACExtensions)
 
 + (RACSignal *)rac_logInWithUsername:(NSString *)username password:(NSString *)password {
-	return [RACSignal createSignal:^RACDisposable * (id<RACSubscriber> subscriber) {
-		[self logInWithUsernameInBackground:username password:password block:PFRACObjectCallback(subscriber)];
-
-		return nil;
-	}];
+	return [[RACSignal
+		createSignal:^RACDisposable * (id<RACSubscriber> subscriber) {
+			[self logInWithUsernameInBackground:username password:password block:PFRACObjectCallback(subscriber)];
+			return nil;
+		}]
+		catch:^(NSError *error) {
+			return [RACSignal error:PFRACDescribeGenericError(error, NSLocalizedString(@"log in failed", nil))];
+		}];
 }
 
 + (RACSignal *)rac_requestPasswordResetForEmail:(NSString *)email {
-	return [RACSignal createSignal:^RACDisposable * (id<RACSubscriber> subscriber) {
-		[self requestPasswordResetForEmailInBackground:email block:PFRACBooleanCallback(subscriber)];
-
-		return nil;
-	}];
+	return [[RACSignal
+		createSignal:^RACDisposable * (id<RACSubscriber> subscriber) {
+			[self requestPasswordResetForEmailInBackground:email block:PFRACBooleanCallback(subscriber)];
+			return nil;
+		}]
+		catch:^(NSError *error) {
+			return [RACSignal error:PFRACDescribeGenericError(error, NSLocalizedString(@"request password reset failed", nil))];
+		}];
 }
 
 - (RACSignal *)rac_signUp {
-	return [RACSignal createSignal:^RACDisposable * (id<RACSubscriber> subscriber) {
-		[self signUpInBackgroundWithBlock:PFRACBooleanCallback(subscriber)];
-
-		return nil;
-	}];
+	return [[RACSignal
+		createSignal:^RACDisposable * (id<RACSubscriber> subscriber) {
+			[self signUpInBackgroundWithBlock:PFRACBooleanCallback(subscriber)];
+			return nil;
+		}]
+		catch:^(NSError *error) {
+			return [RACSignal error:PFRACDescribeGenericError(error, NSLocalizedString(@"sign up failed", nil))];
+		}];
 }
 
 @end
