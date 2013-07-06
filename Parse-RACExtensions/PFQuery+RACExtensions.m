a059c8eb66fd3e56add6389bbf14bab75249084d
Dave Lee <davelee.com@gmail.com>
2013-07-06 00:31:03 -0600

    Remove unnecessary weakify/strongify

--- Parse-RACExtensions/PFQuery+RACExtensions.m
+++ Parse-RACExtensions/PFQuery+RACExtensions.m
@@ -7,7 +7,6 @@
 //
 
 #import <ReactiveCocoa/ReactiveCocoa.h>
-#import <ReactiveCocoa/EXTScope.h>
 
 #import "PFQuery+RACExtensions.h"
 #import "PFRACCallbacks.h"
@@ -15,48 +14,40 @@
 @implementation PFQuery (RACExtensions)
 
 - (RACSignal *)rac_getObjectWithId:(NSString *)objectId {
-	@weakify(self);
 	return [RACSignal createSignal:^(id<RACSubscriber> subscriber) {
 		[self getObjectInBackgroundWithId:objectId block:PFRACObjectCallback(subscriber)];
 
 		return [RACDisposable disposableWithBlock:^{
-			@strongify(self);
 			[self cancel];
 		}];
 	}];
 }
 
 - (RACSignal *)rac_findObjects {
-	@weakify(self);
 	return [RACSignal createSignal:^(id<RACSubscriber> subscriber) {
 		[self findObjectsInBackgroundWithBlock:PFRACObjectCallback(subscriber)];
 
 		return [RACDisposable disposableWithBlock:^{
-			@strongify(self);
 			[self cancel];
 		}];
 	}];
 }
 
 - (RACSignal *)rac_getFirstObject {
-	@weakify(self);
 	return [RACSignal createSignal:^(id<RACSubscriber> subscriber) {
 		[self getFirstObjectInBackgroundWithBlock:PFRACObjectCallback(subscriber)];
 
 		return [RACDisposable disposableWithBlock:^{
-			@strongify(self);
 			[self cancel];
 		}];
 	}];
 }
 
 - (RACSignal *)rac_countObjects {
-	@weakify(self);
 	return [RACSignal createSignal:^(id<RACSubscriber> subscriber) {
 		[self countObjectsInBackgroundWithBlock:PFRACIntegerCallback(subscriber)];
 
 		return [RACDisposable disposableWithBlock:^{
-			@strongify(self);
 			[self cancel];
 		}];
 	}];
