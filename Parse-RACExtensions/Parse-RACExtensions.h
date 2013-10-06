822988a785c0c7f575088da0197ac2a067921c55
Dave Lee <davelee.com@gmail.com>
2013-10-05 19:32:38 -0600

    Remove ReactiveCocoa.h from Parse-RACExtensions.h

--- Parse-RACExtensions/Parse-RACExtensions.h
+++ Parse-RACExtensions/Parse-RACExtensions.h
@@ -6,8 +6,6 @@
 //  Copyright (c) 2013 Dave Lee. All rights reserved.
 //
 
-#import <ReactiveCocoa/ReactiveCocoa.h>
-
 #import "PFAnonymousUtils+RACExtensions.h"
 #import "PFCloud+RACExtensions.h"
 #import "PFFile+RACExtensions.h"
