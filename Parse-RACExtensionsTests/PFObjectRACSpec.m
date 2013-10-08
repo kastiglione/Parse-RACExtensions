5b73308c1dbf4c156a8f23a2ec7545c34d15d192
Dave Lee <davelee.com@gmail.com>
2013-10-08 12:24:13 -0600

    Yay! Tests…
    Only for PFObject save and delete. It's a start.

--- Parse-RACExtensionsTests/PFObjectRACSpec.m
+++ Parse-RACExtensionsTests/PFObjectRACSpec.m
@@ -7,6 +7,8 @@
 //
 
 #import "PFObject+RACExtensions.h"
+#import <ReactiveCocoa/ReactiveCocoa.h>
+#import <Parse/Parse.h>
 
 #import "Specta.h"
 #define EXP_SHORTHAND
@@ -14,10 +16,50 @@
 
 SpecBegin(PFObjectRACSpec)
 
-it(@"should work", ^{
-	PFObject *object = [PFObject objectWithClassName:@"TestRig"];
+beforeAll(^{
+	[Parse setApplicationId:@(getenv("ParseApplicationId")) clientKey:@(getenv("ParseClientKey"))];
+});
+
+afterAll(^{
+	NSArray *testObjects = [[PFQuery queryWithClassName:@"TestObject"] findObjects];
+	[PFObject deleteAll:testObjects];
+});
+
+it(@"should save", ^{
+	PFObject *object = [PFObject objectWithClassName:@"TestObject"];
 	expect(object).notTo.beNil();
-	expect([object respondsToSelector:@selector(rac_save)]).to.beTruthy();
+
+	__block BOOL saved = NO;
+	[[object rac_save] subscribeCompleted:^{
+		saved = YES;
+	}];
+
+	expect(saved).will.beTruthy();
+	expect(object.objectId).willNot.beNil();
+	expect(object.createdAt.timeIntervalSinceReferenceDate).will.beCloseToWithin(NSDate.date.timeIntervalSinceReferenceDate, 300);
+	expect(object.updatedAt).will.equal(object.createdAt);
+});
+
+it(@"should delete", ^{
+	PFObject *object = [PFObject objectWithClassName:@"TestObject"];
+	expect(object).notTo.beNil();
+
+	BOOL saved = [object save];
+
+	expect(saved).to.beTruthy();
+	expect(object.objectId).notTo.beNil();
+
+	__block BOOL deleted = NO;
+	[[object rac_delete] subscribeCompleted:^{
+		deleted = YES;
+
+		PFQuery *query = [PFQuery queryWithClassName:@"TestObject"];
+		[query whereKey:@"objectId" equalTo:object.objectId];
+		expect([query countObjects]).to.equal(0);
+	}];
+
+	expect(deleted).will.beTruthy();
+	expect(object.objectId).willNot.beNil();
 });
 
 SpecEnd
